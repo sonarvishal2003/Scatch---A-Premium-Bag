853d401161664a7fc78c1efd518509f02911aaf6
vishalsonar <sonarvishal860@gmail.com>
2025-06-26 20:17:22 +0530

    first commit

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -11,4 +11,3 @@ mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
     });
 
 module.exports = mongoose.connection;
-// This code connects to a MongoDB database using Mongoose. It logs a success message if the connection is established successfully, and an error message if there is an error during the connection process.
