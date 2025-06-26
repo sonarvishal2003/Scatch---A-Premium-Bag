e79acdf771b20d9990666d2cbc37be81d02f8e9f
vishalsonar <sonarvishal860@gmail.com>
2025-06-26 20:38:10 +0530

    initial commit

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const dbgr = require('debug')('development:mongoose');
 
-mongoose.connect(`${config.get("MONGODB_URI")}/scatch`)
+mongoose.connect(config.get("MONGODB_URI"))
     .then(() => {
         dbgr("MongoDB connection successful");
     })
